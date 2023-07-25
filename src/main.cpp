cada00bada1360edfee6e06099ad9d3064e28aa3
Martin White <11601920+mjwhite8119@users.noreply.github.com>
2023-07-25 15:10:59 -0700

    Simple I2C test works

--- src/main.cpp
+++ src/main.cpp
@@ -1,109 +1,42 @@
-/**
- *
- * Position/angle motion control example
- * Steps:
- * 1) Configure the motor and magnetic sensor
- * 2) Run the code
- * 3) Set the target angle (in radians) from serial terminal
- *
- */
-#include <SimpleFOC.h>
+//
+//    FILE: AS5600_demo.ino
+//  AUTHOR: Rob Tillaart
+// PURPOSE: demo
+//    DATE: 2022-05-28
 
-// magnetic sensor instance - SPI
-// MagneticSensorSPI sensor = MagneticSensorSPI(AS5147_SPI, 10);
-// magnetic sensor instance - MagneticSensorI2C
-MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
-// magnetic sensor instance - analog output
-// MagneticSensorAnalog sensor = MagneticSensorAnalog(A1, 14, 1020);
+#include "AS5600.h"
+#include "Wire.h"
 
-// BLDC motor & driver instance
-BLDCMotor motor = BLDCMotor(7); // Gimbal motor
-BLDCDriver3PWM driver = BLDCDriver3PWM(9, 5, 6, 8);
+AS5600 as5600;   //  use default Wire
 
-// angle set point variable
-float target_angle = 0;
-// instantiate the commander
-Commander command = Commander(Serial);
-void doTarget(char* cmd) { command.scalar(&target_angle, cmd); }
 
-void setup() {
-
-  // initialise magnetic sensor hardware
-  sensor.init();
-  // link the motor to the sensor
-  motor.linkSensor(&sensor);
-
-  // driver config
-  // power supply voltage [V]
-  driver.voltage_power_supply = 8;
-  driver.init();
-  // link the motor and the driver
-  motor.linkDriver(&driver);
-
-  // choose FOC modulation (optional)
-  motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
-
-  // set motion control loop to be used
-  motor.controller = MotionControlType::angle;
-
-  // contoller configuration
-  // default parameters in defaults.h
-
-  // velocity PI controller parameters
-  motor.PID_velocity.P = 0.2f;
-  motor.PID_velocity.I = 20;
-  motor.PID_velocity.D = 0;
-  // maximal voltage to be set to the motor
-  motor.voltage_limit = 6;
-
-  // velocity low pass filtering time constant
-  // the lower the less filtered
-  motor.LPF_velocity.Tf = 0.01f;
-
-  // angle P controller
-  motor.P_angle.P = 20;
-  // maximal velocity of the position control
-  motor.velocity_limit = 20;
-
-  // use monitoring with serial
+void setup()
+{
   Serial.begin(115200);
-  // comment out if not needed
-  motor.useMonitoring(Serial);
+  Serial.println(__FILE__);
+  Serial.print("AS5600_LIB_VERSION: ");
+  Serial.println(AS5600_LIB_VERSION);
 
-
-  // initialize motor
-  motor.init();
-  // align sensor and start FOC
-  motor.initFOC();
-
-  // add target command T
-  command.add('T', doTarget, "target angle");
-
-  Serial.println(F("Motor ready."));
-  Serial.println(F("Set the target angle using serial terminal:"));
-  _delay(1000);
+  //  ESP32
+  //  as5600.begin(14,15);
+  //  AVR
+  as5600.begin(4);  //  set direction pin.
+  as5600.setDirection(AS5600_CLOCK_WISE);  // default, just be explicit.
+  int b = as5600.isConnected();
+  Serial.print("Connect: ");
+  Serial.println(b);
+  delay(1000);
 }
 
 
-void loop() {
+void loop()
+{
+  //  Serial.print(millis());
+  //  Serial.print("\t");
+  Serial.print(as5600.readAngle());
+  Serial.print("\t");
+  Serial.println(as5600.rawAngle());
+  Serial.println(as5600.rawAngle() * AS5600_RAW_TO_DEGREES);
 
-  // main FOC algorithm function
-  // the faster you run this function the better
-  // Arduino UNO loop  ~1kHz
-  // Bluepill loop ~10kHz
-  motor.loopFOC();
-
-  // Motion control function
-  // velocity, position or voltage (defined in motor.controller)
-  // this function can be run at much lower frequency than loopFOC() function
-  // You can also use motor.move() and set the motor.target in the code
-  motor.move(target_angle);
-
-
-  // function intended to be used with serial plotter to monitor motor variables
-  // significantly slowing the execution down!!!!
-  // motor.monitor();
-
-  // user communication
-  command.run();
+  delay(1000);
 }
