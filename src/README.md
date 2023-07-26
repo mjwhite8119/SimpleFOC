95949de05cc63d734e625d0361ecb343c4dedff9
Martin White <11601920+mjwhite8119@users.noreply.github.com>
2023-07-26 13:05:17 -0700

    Closed loop testing

--- src/README.md
+++ src/README.md
@@ -1,7 +1,10 @@
 # SimpleFOC Tests
 This project is setup to do testing of the SimpleFOC library. The `main` branch uses the *SimpleFOCShield* together with an Arduino UNO.  The `SimpleFOCMini-ESP32` branch uses the *SimpleFOCMini* with an ESP32.  Testing on the Arduino UNO is limited to the size of the flash memory so most of the tests will be done on the ESP32.
 
-Tests use a gimbal motor at 7.5V with 7 pole-pairs.  
+Tests use a gimbal motor at 7.5V with 7 pole-pairs.  This was determined by the specs and also verified using the test in the `polepair-test` branch.
+
+Monitoring variables:
+    monitor_variables = _MON_TARGET | _MON_VOLT_Q | _MON_VEL | _MON_ANGLE;
 
 ## Test 1 Open Loop Velocity Control 
 Simple open loop velocity control using `motor.move()`.
@@ -17,5 +20,23 @@ Setup the following commands:
 - L - Set voltage limit
 - S - Set speed limit
 
-## Test 4 - Position Closed Loop
+## Test 4 - Torque Closed Loop
+
+## Test 5 - Velocity Closed Loop
+Got to these parameters without it oscillating:
+    motor.PID_velocity.P = 0.2f;
+    motor.PID_velocity.I = 4.0f;
+    motor.PID_velocity.D = 0;
+
+
+## Test 6 - Position Closed Loop
+Started by getting the parameters for the `motor.initFOC(6.18, Direction::CW);` by using the following lines:
+
+    Serial.println(motor.zero_electric_angle);
+    Serial.println(motor.sensor_direction);
+
+For the gimbal motor this gave me 6.18 and 1 respectively.    
 For the gimbal motor I set `motor.P_angle.P = 8`.  
+
+## Issues
+The motor will suddenly just spin.  No control can stop it other than disconnection the power.  Still not sure how to tune `motor.voltage_limit` and `motor.velocity_limit`.
--- src/main.cpp
+++ src/main.cpp
@@ -22,10 +22,11 @@ BLDCMotor motor = BLDCMotor(7); // Gimbal motor
 BLDCDriver3PWM driver = BLDCDriver3PWM(9, 5, 6, 8);
 
 // angle set point variable
-float target_angle = 0;
+float target = 0;
+
 // instantiate the commander
 Commander command = Commander(Serial);
-void doTarget(char* cmd) { command.scalar(&target_angle, cmd); }
+void doTarget(char* cmd) { command.scalar(&target, cmd); }
 
 void setup() {
 
@@ -44,7 +45,9 @@ void setup() {
   // choose FOC modulation (optional)
   motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
 
-  // set motion control loop to be used
+  // Set motion control loop to be used. Pick only one of these
+  // motor.controller = MotionControlType::torque;
+  // motor.controller = MotionControlType::velocity;
   motor.controller = MotionControlType::angle;
 
   // contoller configuration
@@ -52,41 +55,51 @@ void setup() {
 
   // velocity PI controller parameters
   motor.PID_velocity.P = 0.2f;
-  motor.PID_velocity.I = 20;
+  motor.PID_velocity.I = 4.0f;
   motor.PID_velocity.D = 0;
-  // maximal voltage to be set to the motor
-  motor.voltage_limit = 3;
 
   // velocity low pass filtering time constant
+  // default 5ms - try different values to see what is the best.
   // the lower the less filtered
   motor.LPF_velocity.Tf = 0.01f;
+
+  // maximal voltage to be set to the motor
+  motor.voltage_limit = 3;
 
   // angle P controller
   motor.P_angle.P = 8;
   // maximal velocity of the position control
-  motor.velocity_limit = 10;
+  motor.velocity_limit = 2;
 
   // use monitoring with serial
   Serial.begin(115200);
   // comment out if not needed
   motor.useMonitoring(Serial);
 
-
   // initialize motor
   motor.init();
   // align sensor and start FOC
-  motor.initFOC();
+  motor.initFOC(6.18, Direction::CW);
 
   // add target command T
+  // command.add('T', doTarget, "target voltage");
+  // command.add('T', doTarget, "target velocity");
   command.add('T', doTarget, "target angle");
+
+  // Serial.println(motor.zero_electric_angle);
+  // Serial.println(motor.sensor_direction);
 
   Serial.println(F("Motor ready."));
   Serial.println(F("Set the target angle using serial terminal (Degrees):"));
+  // Serial.println(F("Set the target voltage using serial terminal:"));
+  // Serial.println(F("Set the target velocity using serial terminal (Radians):"));
   _delay(1000);
 }
 
-
+int count = 0;
 void loop() {
+
+  // motor.PID_velocity.I = target;
 
   // main FOC algorithm function
   // the faster you run this function the better
@@ -94,16 +107,23 @@ void loop() {
   // Bluepill loop ~10kHz
   motor.loopFOC();
 
+  // if (count > 1000) {
+  //   Serial.print(motor.PID_velocity.P);Serial.print(" : ");Serial.println(motor.PID_velocity.I);
+  //   count = 0;
+  // }
+  // count++;
+  
   // Motion control function
   // velocity, position or voltage (defined in motor.controller)
   // this function can be run at much lower frequency than loopFOC() function
   // You can also use motor.move() and set the motor.target in the code
-  motor.move(target_angle * (_PI/180)); // Use degrees instead of radians
+  motor.move(target * (_PI/180)); // Use degrees instead of radians
+  // motor.move(target);
 
-
+  // motor.useMonitoring(Serial);
   // function intended to be used with serial plotter to monitor motor variables
   // significantly slowing the execution down!!!!
-  // motor.monitor();
+  motor.monitor();
 
   // user communication
   command.run();
